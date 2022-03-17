fe407ba15ea5c858b96691d7cf352baa59544a49
jolega <alonso_juancho@hotmail.com>
2022-03-17 10:30:46 -0500

    add files callback

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {buscarHeroe} from  './js/callbacks'
+import './styles.css';
+
+const heroeId = 'capi'
+
+buscarHeroe(heroeId, (err, heroe) => {
+
+    if(err){
+        console.error( err );
+    }
+    else{
+        console.info(heroe)
+    }
+
+} );
---
+++ src/js/callbacks.js
@@ -0,0 +1,28 @@
+const heroes = {
+    capi:{
+        nombre :'Capitan America',
+        poder  :'Aguantar inyecciones sin morir'
+    },
+    iron:{
+        nombre :'Iroman',
+        poder  :'Aguantar inyecciones sin morir'
+    },
+    spider:{
+        nombre :'Capitan America',
+        poder  :'Aguantar inyecciones sin morir'
+    },
+
+}
+
+export const buscarHeroe = (id, callback ) =>{
+
+    const heroe = heroes[id]; // selecciona el campo por id
+  
+    if(heroe){
+        callback(null ,heroe);
+    }
+    else{
+        callback(`no existe un heroe con el id ${ id }` );
+    }
+    
+}
