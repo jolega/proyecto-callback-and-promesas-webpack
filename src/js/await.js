255c4e05b236606691c5607b6c74ff274721b5db
jolega <alonso_juancho@hotmail.com>
2022-03-21 08:24:12 -0500

    await con promise all y .map

--- src/js/await.js
+++ src/js/await.js
@@ -1,17 +1,9 @@
-import { buscarHeroeAsync } from './promesas'
+import { buscarHeroeAsync, buscarHeroe } from './promesas'
 
 const heroesIds = [ 'capi', 'iron' , 'spider' ];
 
 export const  obtenerHeroesArr = async () => {
     
-    const heroeArr = [];
+    return await Promise.all (heroesIds.map (heroe => buscarHeroe(heroe) )) ;
 
-    for (const id of heroesIds) {
-       // buscarHeroeAsync(id).then( heroe => heroeArr.push(heroe) );
-       const heroe = await buscarHeroeAsync(id); // await espera que la promesa se resuelva
-       heroeArr.push( heroe) ;
-    }
-    
-
-    return heroeArr;
 }
